5de048e1d69060aaa9b74322ca8194beea1fecc9
MurmuDev <murmudev24@gmail.com>
2020-03-25 21:46:48 +0530

    adding commits

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import numpy as np
+import numpy as np #library for using array objects 
 
 #no if iterations
 n = 150
 
 #function for reflection
-def reflection(ar,x,y,xi,yi):
+def reflection(ar,x,y,xi,yi): #reflection(ar,x,y,x-increment,y-increment)
     xsize = ar.shape[1]
     ysize = ar.shape[0]
-    print(x,y)
     x=x+xi
     y=y+yi
 
-    #calc the diff out of bound point and inverting it wrt to boundary
+    #calculate the different out of bound points and inverting it
     if x<0:
         x=-x
         xi=-xi
@@ -33,13 +32,13 @@ def reflection(ar,x,y,xi,yi):
 
 
 #function for movement iteration
-def move(ar,x,y,xi,yi):
+def move(ar,x,y,xi,yi): #move(array,x,y,x-increment,y-increment)
     ysize = ar.shape[0]
     xsize = ar.shape[1]
     global n
     while n>=0:
        n = n-1
-       if x>=xsize or x<0 or y >=ysize or y<0:
+       if x>=xsize or x<0 or y >=ysize or y<0: #condition to check if out of bounds
            reflection(ar,x-xi,y-yi,xi,yi)
        try:
            ar[y][x] = 1
@@ -50,8 +49,8 @@ def move(ar,x,y,xi,yi):
            print
 
 
-#function for declaring and printing 2D array
-def createarray(y,x):
+#function for declaring 2D array and starting
+def createarray(y,x): #createarray(ysize,xsize)
     ar = np.zeros((y, x))
     move(ar,1,1,1,6)
 
