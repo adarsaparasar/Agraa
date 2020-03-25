082e9c1ae6735171fdab2888d3343f58860d11f7
MurmuDev <murmudev24@gmail.com>
2020-03-25 21:22:08 +0530

    array with reflection (without hash completion check)

--- main.py
+++ main.py
@@ -1,21 +1,63 @@
-import numpy as np  #library for using arrays
+import numpy as np
+
+n = 150
+i = 0
+
+def reflection(ar,x,y,xi,yi):
+    xsize = ar.shape[1]
+    ysize = ar.shape[0]
+    print(x,y)
+    x=x+xi
+    y=y+yi
+
+    #calc the diff out of bound point and inverting it wrt to boundary
+    if x<0:
+        x=-x
+        xi=-xi
+        print("#")
+
+    if y<0:
+        y=-y
+        yi=-yi
+        print("*")
+
+    if x>=xsize:
+        x = xsize-abs((xi+x)-(xsize-1))
+        xi = -xi
+        print("#")
+
+    if y>=ysize:
+        y = ysize-abs((yi+y)-(ysize-1))
+        yi = -yi
+        print("*")
+
+
+    move(ar,x,y,xi,yi)
+
+
+
 def move(ar,x,y,xi,yi):
-    xsize = ar.shape[0]
-    ysize = ar.shape[1]
-    while x<xsize or y<ysize:
-       ar[x][y] = 1
-       print(ar)
-       x = x+xi
-       y = y+yi
+    ysize = ar.shape[0]
+    xsize = ar.shape[1]
+    global n
+    while n>=0:
+       n = n-1
+       if x>=xsize or x<0 or y >=ysize or y<0:
+           reflection(ar,x-xi,y-yi,xi,yi)
+       try:
+           ar[y][x] = 1
+           print(ar)
+           x = x+xi
+           y = y+yi
+       except Exception as e:
+           print
+
 
 #function for declaring and printing 2D array
-def createarray(n):
-    ar = np.zeros((n, n))
-    print(ar.shape[0],ar.shape[1])
-    print(ar)
-    move(ar,0,0,1,1)
+def createarray(y,x):
+    ar = np.zeros((y, x))
+    move(ar,1,1,1,6)
 
-#function call for printarray()
-print("Enter number")
-x = input()
-createarray(int(x))
+#function call for creatarray()
+createarray(6,3)
+print(i)
