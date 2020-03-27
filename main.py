f520275a5ece4b980aa1710acaa515e573acd52f
MurmuDev <murmudev24@gmail.com>
2020-03-27 22:08:20 +0530

    added checkRay() function for checking untraced unit in current path

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import numpy as np #library for using array objects 
-
+import numpy as np #library for using array objects
+from equation import Equation
 #no if iterations
-n = 150
+n = 20
+
+eqList = []
 
 #function for reflection
 def reflection(ar,x,y,xi,yi): #reflection(ar,x,y,x-increment,y-increment)
@@ -30,29 +32,60 @@ def reflection(ar,x,y,xi,yi): #reflection(ar,x,y,x-increment,y-increment)
 
     move(ar,x,y,xi,yi)
 
+#function to check ray re-tracing. Returns 0 if 0 found in current path otherwise 1
+def checkRay(ar,x,y,xi,yi):
+    x = x+xi
+    y = y+yi
+    xsize = ar.shape[1]
+    ysize = ar.shape[0]
+    while x>=0 and y>=0 and x<xsize and y<ysize:
+        print(x,y)
+        if ar[y][x] == 0:
+            return 0
+        x = x + xi
+        y = y + yi
+    return 1
+
+
+
 
 #function for movement iteration
 def move(ar,x,y,xi,yi): #move(array,x,y,x-increment,y-increment)
     ysize = ar.shape[0]
     xsize = ar.shape[1]
     global n
+
+    #creating equation object using Equation
+    eq = Equation(x,y,xi,yi)
+    eq = eq.getLine()
+
+
     while n>=0:
-       n = n-1
-       if x>=xsize or x<0 or y >=ysize or y<0: #condition to check if out of bounds
-           reflection(ar,x-xi,y-yi,xi,yi)
-       try:
-           ar[y][x] = 1
-           print(ar)
-           x = x+xi
-           y = y+yi
-       except Exception as e:
-           print
+        if x>=xsize or x<0 or y >=ysize or y<0: #condition to check if out of bounds
+            reflection(ar,x-xi,y-yi,xi,yi)
+        try:
+            eqList.append(eq)
+            n = n-1
+            print(eq)
+            ar[y][x] = 1
+            print(ar)
+            x = x+xi
+            y = y+yi
+        except Exception as e:
+            print
+
 
 
 #function for declaring 2D array and starting
 def createarray(y,x): #createarray(ysize,xsize)
     ar = np.zeros((y, x))
-    move(ar,1,1,1,6)
+    move(ar,1,1,1,1)
 
 #function call for creatarray()
-createarray(6,3)
+try:
+    createarray(6,3)
+except Exception:
+    print
+print("*")
+for i in eqList:
+    print(i)
